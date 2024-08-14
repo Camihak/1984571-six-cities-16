07877d9c1cb7104df90886175ff018036401bdaf
Камила <105364337+Camihak@users.noreply.github.com>
2024-08-14 14:19:20 +0300

    Описывает тестовые данные для предложений по аренде недвижимости

---
+++ src/mocks/offers.ts
@@ -0,0 +1,97 @@
+import { Offers } from "../types/offer"
+
+export const offers: Offers = [
+  {
+    "id": "6af6f71154676782a27f00",
+    "title": "Beautiful studio",
+    "type": "apartment",
+    "price": 150,
+    "city": {
+    "name": "Amsterdam",
+    "location": {
+    "latitude": 52.355178438634686378,
+    "longitude": 4.67387464638463948,
+    "zoom": 7
+    }
+    },
+    "location": {
+    "latitude": 52.355178438634686378,
+    "longitude": 4.67387464638463948,
+    "zoom": 7
+    },
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 4,
+    "previewImage": "https://url-to-image/image.png"
+  },
+  {
+    "id": "6af6f711834792682a27f00",
+    "title": "luxurious studio",
+    "type": "flat",
+    "price": 120,
+    "city": {
+    "name": "Moscow",
+    "location": {
+    "latitude": 52.37845636736738738,
+    "longitude": 4.8934736754543,
+    "zoom": 6
+    }
+    },
+    "location": {
+    "latitude": 52.37845636736738738,
+    "longitude": 4.8934736754543,
+    "zoom": 6
+    },
+    "isFavorite": true,
+    "isPremium": false,
+    "rating": 3,
+    "previewImage": "https://url-to-image/image.png"
+  },
+  {
+    "id": "6af6f71784682727462a27f00",
+    "title": "Beautiful & luxurious studio at great location",
+    "type": "apartment",
+    "price": 200,
+    "city": {
+    "name": "Paris",
+    "location": {
+    "latitude": 51.35514938496378,
+    "longitude": 2.673877537499948,
+    "zoom": 2
+    }
+    },
+    "location": {
+    "latitude": 51.35514938496378,
+    "longitude": 2.673877537499948,
+    "zoom": 2
+    },
+    "isFavorite": true,
+    "isPremium": false,
+    "rating": 4,
+    "previewImage": "https://url-to-image/image.png"
+  },
+  {
+    "id": "6af6f71173834684345b462a27f00",
+    "title": "Wondeful place",
+    "type": "hostel",
+    "price": 50,
+    "city": {
+    "name": "Kursk",
+    "location": {
+    "latitude": 50.35514938496378,
+    "longitude": 1.673877537499948,
+    "zoom": 5
+    }
+    },
+    "location": {
+    "latitude": 50.35514938496378,
+    "longitude": 1.673877537499948,
+    "zoom": 5
+    },
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 5,
+    "previewImage": "https://url-to-image/image.png"
+  }
+]
+
